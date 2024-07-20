fa17f251e5667afb8f3d932a7296ba9d5da12461
Hoang Tien Anh <hoangtienanh2k3@gmail.com>
2024-07-20 11:38:48 +0700

    update add model embedding

--- evaluate_embedding.py
+++ evaluate_embedding.py
@@ -5,16 +5,15 @@ except:
     os.system('pip install -U sentence-transformers')
     from sentence_transformers import SentenceTransformer
     
-import torch.nn.functional as F
-import torch
 
 from sentence_transformers import SentenceTransformer
 import torch
 import torch.nn.functional as F
+import argparse
 
-def compare_gt_with_pred(gt, pred, model_name="hiieu/halong_embedding", truncate_dim=64): #matryoshka_dim = 64
+def compare_gt_with_pred(gt, pred, model_name="hiieu/halong_embedding"):
     # Khởi tạo model
-    model = SentenceTransformer(model_name, truncate_dim=truncate_dim)
+    model = SentenceTransformer(model_name)
     
     # Encode ground_truth và prediction
     gt_embedding = model.encode(gt)
@@ -25,9 +24,15 @@ def compare_gt_with_pred(gt, pred, model_name="hiieu/halong_embedding", truncate
     
     return similaritiy.item()
 
+def arg_parser():
+    parser = argparse.ArgumentParser(description='Evaluate embedding similarity')
+    parser.add_argument('--model_name', type=str, default="hiieu/halong_embedding", help='Model name : dangvantuan/vietnamese-embedding, VoVanPhuc/sup-SimCSE-VietNamese-phobert-base, hiieu/halong_embedding')
+    args = parser.parse_args()
+    return args
 
-ground_truth='Một người đàn ông mặc tạp dề đầu bếp đang đứng trong bếp, trước một chiếc lò nướng màu đen. Ông đang cầm một chiếc chảo trong một tay và một chiếc thìa trong tay kia. Sau lưng ông là một chiếc bàn có nhiều loại dụng cụ nấu ăn khác nhau. Trên bàn là một số bát, cốc và dụng cụ. Ở phía bên trái của bàn là một chiếc lò nướng khác và một cái chảo lớn hơn.\n\nCạnh bàn là một người khác, mặc áo sơ mi và quần tây. Người này dường như đang quan sát người đầu bếp làm bánh pizza. Trong góc bên phải của khung cảnh, có một bồn rửa lớn với vòi nước.',
+ground_truth='Một người đàn ông mặc tạp dề đầu bếp đang đứng trong bếp, trước một chiếc lò nướng màu đen. Ông đang cầm một chiếc chảo trong một tay và một chiếc thìa trong tay kia. Sau lưng ông là một chiếc bàn có nhiều loại dụng cụ nấu ăn khác nhau. Trên bàn là một số bát, cốc và dụng cụ. Ở phía bên trái của bàn là một chiếc lò nướng khác và một cái chảo lớn hơn.\n\nCạnh bàn là một người khác, mặc áo sơ mi và quần tây. Người này dường như đang quan sát người đầu bếp làm bánh pizza. Trong góc bên phải của khung cảnh, có một bồn rửa lớn với vòi nước.'
 prediction="Hình ảnh cho thấy một người đàn ông mặc tạp dề đang đứng trong bếp. Anh ta đang đứng trước một cái bàn, trên đó có nhiều dụng cụ nấu ăn khác nhau. Người đàn ông đang cầm một cái thìa và một cái nĩa, có vẻ như anh ta đang chuẩn bị nấu ăn.\nCó một số vật dụng nhà bếp khác trong bếp, bao gồm một cái bát, một cái cốc và một cái thìa khác. Một cái bát khác có thể được nhìn thấy trên một cái bàn gần đó.\nCó một số vật dụng khác trong bếp, chẳng hạn như một cái lò nướng, một cái lò vi sóng và một số tủ."
 
-similarity = compare_gt_with_pred(ground_truth, prediction)
+args = arg_parser()
+similarity = compare_gt_with_pred(ground_truth, prediction, args.model_name)
 print(similarity)
